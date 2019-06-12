addfe51367412ff6e39fcbb0e1074f45954d2979
John Moshakis <john.moshakis@firmex.com>
2019-06-11 23:12:08 -0400

    Example code

---
+++ RedisConsoleApplication/Program.pas
@@ -0,0 +1,36 @@
+﻿namespace RedisConsoleApplication;
+
+uses
+  StackExchange.Redis;
+type
+  Program = class
+  public
+
+    class method Main(args: array of String): Int32;
+    begin
+      var redis := ConnectionMultiplexer.Connect('localhost:6379');
+
+      var db := redis.GetDatabase;
+
+      db.StringSet('myKey','someValue 2',new TimeSpan(0,0,10));
+
+      var value := db.StringGet('myKey');
+
+      if(assigned(value))then
+      begin
+        Console.WriteLine($'value is {value}');
+      end
+      else
+      begin
+        Console.WriteLine('No value');
+      end;
+
+      Console.WriteLine('Press any key to stop...');
+      Console.ReadLine;
+
+
+    end;
+
+  end;
+
+end.
---
+++ RedisExpiryConsoleApplication/Program.pas
@@ -0,0 +1,35 @@
+﻿namespace RedisExpiryConsoleApplication;
+
+uses
+  StackExchange.Redis;
+
+type
+  Program = class
+  public
+
+    class method Main(args: array of String): Int32;
+    begin
+      var redis := ConnectionMultiplexer.Connect('localhost:6379');
+
+      var subscriber := redis.GetSubscriber;
+
+      var expiredChannel := '__keyevent@0__:expired';
+
+      subscriber.Subscribe(expiredChannel, (channel, value) ->
+          begin
+            Console.WriteLine($'Expired channel {channel} value {value}');
+            /*
+            if ((string)channel == "__keyspace@0__:users" && (string)value == "sadd")
+            {
+                // Do stuff if some item is added to a hypothethical "users" set in Redis
+            }
+            */
+          end);
+
+        Console.WriteLine('Any key to exit');
+        Console.ReadLine;
+    end;
+
+  end;
+
+end.
